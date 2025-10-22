217c9be6ab86b809f8a5f17deec7ceaabebdea43
Jose Leonel Salazar Tejeda <jsalazart3@miumg.edu.gt>
2025-10-22 08:37:40 -0600

    Update environment.ts

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,11 +1,9 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiBaseLogin: 'https://apilogin.mundoalonzo.com',
-  apiBase: 'https://uniondeprofesionales.com/'
-=======
+
+
   apiBaseLogin: 'https://uniondeprofesionales.com/auth/', // con / final
   apiBase: 'https://uniondeprofesionales.com/api/'         // SIN / final, pero ya incluye /api
->>>>>>> 398cb06 (Actualizaciones realizadas directamente desde la VPS (auth, environment, docker-compose, etc.))
+
 };
 
